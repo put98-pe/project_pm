a6d9e450d113bc0b75e69343bbf1358defb56ea7
put98-pe <cx_bigdata_pe@126.com>
2020-08-20 00:00:51 +0800

    try05 conflict

--- src/main/java/com/atguigu/project/emp.java
+++ src/main/java/com/atguigu/project/emp.java
@@ -5,6 +5,6 @@ public class emp {
         System.out.println("emp.java");
         System.out.println("nothing");
         System.out.println("nothing2");
-        System.out.println("nothing3");
+        System.out.println("nothing4");
     }
 }

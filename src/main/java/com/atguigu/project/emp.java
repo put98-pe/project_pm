7d77f0d66cc2f58196556932ad0687d548a94632
cx <cx_bigdata@126.com>
2020-08-19 22:14:14 +0800

    try02 by idea from project_pm

--- src/main/java/com/atguigu/project/emp.java
+++ src/main/java/com/atguigu/project/emp.java
@@ -3,5 +3,6 @@ package com.atguigu.project;
 public class emp {
     public static void main(String[] args) {
         System.out.println("emp.java");
+        System.out.println("nothing");
     }
 }

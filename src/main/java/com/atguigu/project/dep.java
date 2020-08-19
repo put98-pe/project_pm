864565b02014aa8dc866d3d5adbe765a056e9e2c
put98-pe <cx_bigdata_pe@126.com>
2020-08-19 22:22:09 +0800

    try02 by idea from project_pm

--- src/main/java/com/atguigu/project/dep.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.project;
-
-public class dep {
-    public static void main(String[] args) {
-        System.out.println("dep.java");
-    }
-}
--- src/main/java/com/atguigu/project/emp.java
+++ src/main/java/com/atguigu/project/emp.java
@@ -4,5 +4,6 @@ public class emp {
     public static void main(String[] args) {
         System.out.println("emp.java");
         System.out.println("nothing");
+        System.out.println("nothing2");
     }
 }
